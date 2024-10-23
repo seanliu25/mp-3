3d6e920d8ab828cc3371029ccfeafdd16eaaf135
Shangwei Liu <shangweiliu@Macintosh.local>
2024-10-22 21:14:48 -0400

    footer

--- src/assets/components/footer.tsx
+++ src/assets/components/footer.tsx
@@ -1,10 +1,11 @@
 import { STYFOOTER } from './mystyle';
+import { Link } from 'react-router-dom';
 
 export default function Footer(){
     return(
         <STYFOOTER>
             <footer>
-                <p> All rights reserved by Shangwei Liu <a href="/credit">Credits</a> &#169;</p>
+                <p> All rights reserved by Shangwei Liu <Link to = "/credit">Credit</Link> &#169;</p>
             </footer>
         </STYFOOTER>
     )
